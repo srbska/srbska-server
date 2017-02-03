5ca8dc99327b4854a4fc6c1fdb23961c30da64d3
edsilv <e.silverton@gmail.com>
2017-02-03 15:37:23 +0000

    video

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    
+    socket.on('chatMessage', function(msg){
+        io.emit('chatMessage', msg);
+    });
+
+    socket.on('sceneChanged', function(obj){
+        io.emit('sceneChanged', obj);
+    });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
