216a2d207016cafbc1b573d1d7f387b01b3ff8ac
edsilv <e.silverton@gmail.com>
2017-02-04 12:37:02 +0000

    port 80 for video

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.argv[2] || 3000;
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -20,6 +22,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
